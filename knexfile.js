bbcbcac090cfc7028a85505375f51c7f565bf95e
masassuncao <marcoassuncao.eng@gmail.com>
2023-10-29 21:29:25 -0300

    Correção conexão

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,8 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './database/dev.sqlite3'
+      filename: './database/dev.sqlite3',
+      useNullAsDefault: true
     },
     migrations: {
       directory: './database/migrations',
