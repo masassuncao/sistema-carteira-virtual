6c9c86638e2ef135ed0d1587329f546ac6182e55
masassuncao <marcoassuncao.eng@gmail.com>
2023-11-03 00:52:31 -0300

    Ajuste knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './database/dev.sqlite3',
-      useNullAsDefault: true
+      filename: './database/dev.sqlite3'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'knex_migrations'
